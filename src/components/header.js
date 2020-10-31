f8967b539ee2e89f7386c4a20ea676bfc5c60e76
vincentslee <911vincentlee@gmail.com>
2020-10-31 15:57:07 -0500

    Added header html

---
+++ src/components/header.js
@@ -0,0 +1,47 @@
+function Header(){
+    return (
+
+<header>
+  <div class="profileLogo"> 
+
+    <p class="logoPlaceholder">
+        <img src="logoImage.png" alt="sample logo"><span>LOGO</span></img>
+    </p>
+  </div>
+  <div class="profilePhoto"> 
+
+    <img src="../AboutPageAssets/images/profilephoto.png" alt="sample"></img>
+    </div>
+
+  <div class="profileHeader">
+    <h1>JOHN DOE</h1>
+    <h3>REALLY AWESOME WEB DESIGNER</h3>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in.</p>
+  </div>
+
+  <aside class="socialNetworkNavBar">
+    <div class="socialNetworkNav"> 
+
+      <img src="../AboutPageAssets/images/social.png" alt="sample"></img>
+      </div>
+    <div class="socialNetworkNav"> 
+
+      <img src="../AboutPageAssets/images/social.png"  alt="sample"> </img>
+      </div>
+    <div class="socialNetworkNav"> 
+
+      <img src="../AboutPageAssets/images/social.png"  alt="sample"> </img>
+      </div>
+    <div class="socialNetworkNav"> 
+
+      <img src="../AboutPageAssets/images/social.png"  alt="sample"> </img>
+      </div>
+  </aside>
+</header>
+
+
+    )
+}
+
+export default Header;
