0d7d2af22e1bb34634226fcb898703a6b0e1ee32
vincentslee <911vincentlee@gmail.com>
2020-10-31 15:44:44 -0500

    Added project html

---
+++ src/components/project.js
@@ -0,0 +1,44 @@
+function project(){
+    return (
+        <div class="mainContent"> 
+
+        <div class="div1">
+            <h2 class="divTitle">Content Holder 1</h2>
+
+            <div class="div1Content">
+            <p><span>Email :</span> johndoe@email.com</p>
+            <p><span>Website :</span> johndoe.com</p>
+            <p><span>Phone :</span> (123)456 - 789000</p>
+            <p><span>Address :</span> Anytown, Anycountry</p>
+            </div>
+        </div>
+
+        <div class="div2">
+            <h2 class="divTitle">Content Holder 2</h2>
+
+
+            <article class="div2Content">
+            <h2 class="divContentTitle">Title & Company</h2>
+            <h3 class="divContentSubTitle">Position / Date - Year</h3>
+            <p class="divContent"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. </p>
+            </article>
+
+            <article class="div2Content">
+            <h2 class="divContentTitle"> Title & Company</h2>
+            <h3 class="divContentSubTitle">Position / Date - Year</h3>
+            <p class="divContent"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. </p>
+            </article>
+        
+        </div>
+
+        <aside class="externalResourcesNav">
+            <div class="externalResources"> <a href="#" title="Download CV Link">DOWNLOAD CV</a> </div>
+            <span class="stretch"></span>
+            <div class="externalResources"><a href="#" title="Behance Link">BEHANCE</a> </div>
+            <span class="stretch"></span>
+            <div class="externalResources"><a href="#" title="Github Link">GITHUB</a> </div>
+        </aside>
+        </div>
+
+    );
+}
