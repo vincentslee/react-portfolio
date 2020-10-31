c77a245c0fee1ee6d50973ca7567bf89cd39afb9
Vincent Lee <911vincentlee@gmail.com>
2020-10-31 15:51:37 -0500

    Debugged DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Project from './components/Project.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Project />
   </React.StrictMode>,
   document.getElementById('root')
 );
